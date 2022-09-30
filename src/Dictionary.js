a7e764230d0049892147c9bb4782d4c6798aeee9
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-09-30 14:58:32 -0300

    call API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Looking for ${keyWord}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function storeInputForm(event) {
-    console.log(event.target.value);
     setKeyWord(event.target.value);
   }
 

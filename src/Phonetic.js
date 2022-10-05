f6f1c68b32237475cce4788f5910f273cde60470
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-10-05 16:43:27 -0300

    add noreferrer to sound

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   console.log(props);
   return (
     <div>
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" without rel="noreferrer">
         🔈
       </a>
       <br />
